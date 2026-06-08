e429fd43d1f732c7ec6e6896112b9cf131feff4d
Homnk-U <juan.daniel.riv@gmail.com>
2026-06-07 19:37:56 -0600

    Corrección de detalles

--- src/main.py
+++ src/main.py
@@ -5,11 +5,11 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from PySide6.QtWidgets import QApplication
-from gui.main_window import MainWindow  # <-- Cambiamos la importación
+from gui.main_window import MainWindow
 
 def main():
     app = QApplication(sys.argv)
-    window = MainWindow()               # <-- Arrancamos la ventana contenedora
+    window = MainWindow()
     window.show()
     sys.exit(app.exec())
 
